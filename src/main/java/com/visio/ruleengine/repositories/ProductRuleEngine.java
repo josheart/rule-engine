6a8dc33ed60d2f7ee0241e1b15c046e39e096251
josheart <cesurthesdet@gmail.com>
2020-01-20 05:49:17 -0600

    removed Getter annotation

--- src/main/java/com/visio/ruleengine/repositories/ProductRuleEngine.java
+++ src/main/java/com/visio/ruleengine/repositories/ProductRuleEngine.java
@@ -1,9 +1,7 @@
 package com.visio.ruleengine.repositories;
 
-import lombok.Getter;
 import org.springframework.stereotype.Repository;
 
-@Getter
 @Repository
 public class ProductRuleEngine implements RuleEngine {
 }
