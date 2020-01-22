ee3d905a6185ea4aa6fc251a3c1dc9a8301021c1
josheart <cesurthesdet@gmail.com>
2020-01-21 20:35:12 -0600

    added comments to controller and cleaned the method

--- src/main/java/com/visio/ruleengine/controllers/ProductRuleController.java
+++ src/main/java/com/visio/ruleengine/controllers/ProductRuleController.java
@@ -1,35 +1,40 @@
 package com.visio.ruleengine.controllers;
 import com.visio.ruleengine.models.PersonProductPair;
 import com.visio.ruleengine.models.Product;
-import com.visio.ruleengine.services.ProductRuleService;
 import com.visio.ruleengine.services.RuleService;
-import com.visio.ruleengine.views.CreditScore;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.net.URISyntaxException;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/api/v1/productPrice")
 public class ProductRuleController {
     private static final Logger LOG = LoggerFactory.getLogger(ProductRuleController.class);
 
     private final RuleService ruleService;
 
-    @PostMapping
+    /**
+     * Returns the productPrice and eligibility
+     * @param personProductPair The demographic data of the person, initial interest rate and requested product
+     * @param bindingResult catches any error happens during binding
+     * @return the productPrice and eligibility
+     */
+    @PostMapping("/productPrice")
     ResponseEntity<Product> submit(@Valid @RequestBody final PersonProductPair personProductPair, BindingResult bindingResult){
         if(bindingResult.hasErrors()) {
             LOG.warn("Validation failed: {}", bindingResult);
             return new ResponseEntity<>( HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(ruleService.getProductPricing(personProductPair), HttpStatus.OK);
-
+        Product newProduct = ruleService.getProductPricing(personProductPair);
+        return ResponseEntity
+                    .ok()
+                    .body(newProduct);
     }
 }
