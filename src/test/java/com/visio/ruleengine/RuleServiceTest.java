cb1098e4422d4f31c485e33fb801079316f0c03d
josheart <cesurthesdet@gmail.com>
2020-01-23 00:41:46 -0600

    removed unused import

--- src/test/java/com/visio/ruleengine/RuleServiceTest.java
+++ src/test/java/com/visio/ruleengine/RuleServiceTest.java
@@ -13,7 +13,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.doReturn;
 
 @ExtendWith(SpringExtension.class)
