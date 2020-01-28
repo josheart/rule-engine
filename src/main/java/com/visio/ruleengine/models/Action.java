14c21ae110c05ceacfb86373c35aff148a7c9cf5
josheart <cesurthesdet@gmail.com>
2020-01-27 20:58:28 -0600

    added ToString anno to Action and Condition

--- src/main/java/com/visio/ruleengine/models/Action.java
+++ src/main/java/com/visio/ruleengine/models/Action.java
@@ -3,12 +3,14 @@ package com.visio.ruleengine.models;
 import com.visio.ruleengine.rules.action.ActionType;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 /**
  *
  */
 @Getter
 @Setter
+@ToString
 public class Action {
     private String key;
     private String value;
--- src/main/java/com/visio/ruleengine/models/Condition.java
+++ src/main/java/com/visio/ruleengine/models/Condition.java
@@ -3,9 +3,11 @@ package com.visio.ruleengine.models;
 import com.visio.ruleengine.rules.condition.ConditionType;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class Condition {
     private String key;
     private String value;
